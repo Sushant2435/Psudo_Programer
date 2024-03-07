46a922b25e6f51b5974196b4473bd18c1280985c
Sushant <sharmasushant245@gmail.com>
2024-03-07 23:30:59 +0530

    update

---
+++ JavaScript Interview questions/tout1.js
@@ -0,0 +1,42 @@
+// number  64 bit
+// let a = 10;
+// let b = 10.4;
+// console.log(typeof (b))
+
+//bigInt
+// let a = 1234353454564564n;
+// console.log(typeof (a))
+
+//String 
+// let a = 'Sushant';
+// let b = "Sushant";
+// console.log(typeof (b))
+
+//boolen
+// true and false ==  1 || 0
+
+// undefined 
+
+// let a;
+// console.log(a);
+
+//null
+
+// let b = null;
+// console.log(b);
+
+// non-primitive
+
+// object 
+
+let obj = {
+    name: "Sushant",
+    age: 23,
+    class:null
+}
+console.log(obj.name)
+
+let arr = [1,2,4,5,"Sushant"]
+arr[1];
+
+
---
+++ JavaScript Interview questions/tout2.js
@@ -0,0 +1,65 @@
+// 1.Global scope
+// 2. local function scope
+// 3. block scope
+
+// let a = 10;
+// function func() {
+//     var b = 20;
+//     console.log(b);
+
+
+// }
+
+// func();
+
+// {
+//     let a =10
+
+// }
+
+// 1. scope of the variable 
+// 2. Redecalaration  and reassign
+// 3. Hoisting
+
+// var...... Local or function and global
+
+// var a = 10;
+
+// function sum() {
+//     var b = a + a; // 20
+//     console.log(b)
+//     if (a < 20) {
+//         let c = 30;
+//     }
+//     console.log(c);
+// }
+
+// sum();
+// var a = 10;
+// var a = 30;
+// console.log(a)
+
+// console.log(c); // undefined
+// let c = 30;
+
+// let & const...... local or function , global, block;
+
+// const a = 20;
+// function sum() {
+//     const b = a + a;
+//     console.log(b);
+//     if (true) {
+//         const c = 50;
+
+//     }
+//     console.log(c)
+// }
+// sum();
+
+// const a = 10;
+// const a = 20;
+// console.log(a)
+
+// console.log(b);
+// const b = 10;
+
